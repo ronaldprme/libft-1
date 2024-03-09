b1595b7dade64de343f6a846d0bd488cc2a878e7
Tiago-0liveira <tfgoliveira@gmail.com>
2024-03-09 14:48:15 +0000

    fixed ft_strtrim erroring when all chars where to be eliminated

--- str/ft_strtrim.c
+++ str/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:33:01 by tiagoliv          #+#    #+#             */
-/*   Updated: 2024/02/23 16:13:00 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2024/03/09 14:46:28 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		c;
 	int		st;
 	int		en;
 	char	*r;
 
-	c = 0;
-	st = 0;
-	en = 0;
 	if (!s1)
 		return (NULL);
-	while (s1[c] && ft_strchr(set, s1[c]))
-		c++;
-	st = c;
-	c = ft_strlen(s1) - 1;
-	while (s1[c] && ft_strchr(set, s1[c]))
-		c--;
-	en = c + 1;
-	if (st > en)
-		st = en;
-	r = (char *)malloc(sizeof(char) * (en - st + 1));
+	st = 0;
+	while (s1[st] && ft_strchr(set, s1[st]))
+		st++;
+	if (st == (int) ft_strlen(s1))
+		en = st;
+	else
+	{
+		en = ft_strlen(s1) - 1;
+		while (en > st && s1[en] && ft_strchr(set, s1[en]))
+			en--;
+	}
+	r = (char *)malloc(sizeof(char) * (en - st + 1) + 1);
 	if (!r)
 		return (NULL);
-	ft_strlcpy(r, s1 + st, en - st + 1);
+	ft_strlcpy(r, s1 + st, en - st + 1 + 1);
 	return (r);
 }
